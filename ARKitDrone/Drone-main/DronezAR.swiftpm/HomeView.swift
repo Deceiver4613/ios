ed3e16c2684f4267d7a98bb5ef4cc6a6179a496d
Deceiver4613 <129382938+Deceiver4613@users.noreply.github.com>
2023-11-25 17:31:26 +0900

    Update HomeView.swift

--- ARKitDrone/Drone-main/DronezAR.swiftpm/HomeView.swift
+++ ARKitDrone/Drone-main/DronezAR.swiftpm/HomeView.swift
@@ -5,63 +5,17 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.black
-                    .ignoresSafeArea()
-                ZStack{}
-                    .frame( maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Image("bg").resizable())
-                    .ignoresSafeArea()
-                VStack {
-                    Image("profile")
-                        .resizable()
-                        .frame(width: 300, height: 300)
-                    Text("Hii! 👋🏻 I'am Ayush Singh")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .padding()
-                        .foregroundColor(.white)
-                    Text("I'm an astute App, Web Developer, and Designer with a passion for ideation, design, and creation of innovative products. I'm also an Apple  WWDC 2021 & 2022 Swift Student Challenge winner")
-                        .font(.system(size: 15))
-                        .fontWeight(.medium)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 60)
-                        .padding(.vertical)
-                        .foregroundColor(.gray)
-                    
-                    HStack{
-                        NavigationLink(destination: UserManual()) {
-                            
-                                HStack {
-                                    Image(systemName: "eyeglasses")
-                                        .foregroundColor(.black)
-                                    Text("Watch my WWDC23 Project")
-                                        .foregroundColor(.black)
-                                        .fontWeight(.bold)
-                                }
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(30)
-                                
-                            
-                        }
-                        
-                    }
-
-                }
-            }
-        }.navigationViewStyle(StackNavigationViewStyle())
+    static var previews: some View {
+        UserManual()
     }
 }
-
+/*
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         UserManual()
     }
 }
-
+*/
 
 
 
