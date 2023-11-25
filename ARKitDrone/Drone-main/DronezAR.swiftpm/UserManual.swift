82181a663e8cba1ed2c3d3e693bf1644c0466a50
Deceiver4613 <129382938+Deceiver4613@users.noreply.github.com>
2023-11-25 17:11:18 +0900

    Update UserManual.swift

--- ARKitDrone/Drone-main/DronezAR.swiftpm/UserManual.swift
+++ ARKitDrone/Drone-main/DronezAR.swiftpm/UserManual.swift
@@ -2,35 +2,28 @@ import SwiftUI
 
 struct UserManual: View {
     var body: some View {
-        // ZStack은 뷰를 겹쳐서 표시하는 데 사용됩니다.
         ZStack {
-            Color.white // ZStack의 배경색상 (이 경우 흰색).
-
-            // 다른 ZStack은 전체 화면을 채우고 배경 이미지를 설정합니다.
+            Color.white
             ZStack{}
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .frame( maxWidth: .infinity, maxHeight: .infinity)
                 .background(Image("bg").resizable())
                 .ignoresSafeArea()
-
-            // VStack은 뷰를 수직으로 배열하는 데 사용됩니다.
+            
             VStack(spacing: 30) {
-                // HStack은 이미지 로고("DronezAR")를 표시합니다.
                 HStack {
                     Image("DronezAR")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 300)
                 }.padding()
-
-                // 드론 이미지를 나타냅니다.
                 Image("drone")
                     .resizable()
                     .frame(width: 400, height: 250)
-
-                // NavigationLink를 사용하여 탭하면 'HowToPlayView'로 전환됩니다.
                 NavigationLink(destination: HowToPlayView()) {
+                    
+                    
                     HStack {
-                        Text("사용 방법")
+                        Text("How to use")
                             .foregroundColor(.white)
                             .fontWeight(.bold)
                     }
@@ -41,12 +34,14 @@ struct UserManual: View {
                         RoundedRectangle(cornerRadius: 30)
                             .stroke(Color.white, lineWidth: 2)
                     )
+                    
+                    
                 }
-
-                // NavigationLink를 사용하여 탭하면 'DroneARView'로 전환됩니다.
+                
                 NavigationLink(destination: DroneARView()) {
+                    
                     HStack {
-                        Text("날아가기")
+                        Text("Go Fly")
                             .foregroundColor(.black)
                             .fontWeight(.bold)
                     }
@@ -54,18 +49,21 @@ struct UserManual: View {
                     .frame(width: 300)
                     .background(.white)
                     .cornerRadius(30)
+                    
+                    
+                    
                 }
-
-                // NavigationLink를 사용하여 탭하면 'DemoVideo'로 전환됩니다.
                 NavigationLink(destination: DemoVideo()) {
                     HStack {
-                        Text("데모 시청")
+                        Text("Watch Demo")
                             .foregroundColor(.white)
                             .fontWeight(.bold)
                             .underline()
                     }
                 }
+                
             }
+            
         }
     }
 }
