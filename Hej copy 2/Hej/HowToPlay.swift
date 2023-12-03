012a3c7242d40a179ff0663e50dfb14be64c72a2
min4034415 <min4034415@gmail.com>
2023-12-03 14:22:31 +0900

    한글로 바꿈

--- Hej copy 2/Hej/HowToPlay.swift
+++ Hej copy 2/Hej/HowToPlay.swift
@@ -17,10 +17,10 @@ struct HowToPlayView: View {
                 .navigationTitle("How to use")
                 
             TabView {
-                OnboardingStepView(image: "step1", title: "Power On", description: "To power on the drone, press the Power On button on the app. Once the drone is turned on, its indicator lights will turn to green.")
-                OnboardingStepView(image: "step2", title: "Left Joystick", description: "Use the left joystick to control the drone's altitude and rotation. Push the joystick up to make the drone ascend, and push it down to make it descend. You can also rotate the drone by moving the joystick left or right.")
-                OnboardingStepView(image: "step3", title: "Right Joystick", description: "Use the right joystick to control the drone's movement. Push the joystick forward to make the drone move forward, and pull it back to make it move backward. You can also move the drone left or right by moving the joystick left or right.")
-                OnboardingStepView(image: "step4", title: "Back To Position", description: "If you need to pause the drone's movement and go back to its original location, press the Pause button on the app. The drone will automatically return to its takeoff point.")
+                OnboardingStepView(image: "step1", title: "전원 켜기", description: "드론의 전원을 켜려면 앱에서 전원 켜기 버튼을 누르세요. 드론이 켜지면 표시등이 녹색으로 변합니다.")
+                OnboardingStepView(image: "step2", title: "왼쪽 조이스틱", description: "드론의 고도와 회전을 제어하려면 왼쪽 조이스틱을 사용하세요. 조이스틱을 위로 밀면 드론이 상승하고, 아래로 밀면 하강합니다. 또한 조이스틱을 왼쪽이나 오른쪽으로 움직여 드론을 회전시킬 수 있습니다.")
+                OnboardingStepView(image: "step3", title: "오른쪽 조이스틱", description: "드론의 이동을 제어하려면 오른쪽 조이스틱을 사용하세요. 조이스틱을 앞으로 밀면 드론이 전진하고, 뒤로 당기면 드론이 후진합니다. 또한 조이스틱을 왼쪽이나 오른쪽으로 움직여 드론을 좌우로 이동시킬 수 있습니다.")
+                OnboardingStepView(image: "step4", title: "원위치로 돌아가기", description: "드론의 이동을 일시 중지하고 원래 위치로 돌아가려면 앱에서 일시 중지 버튼을 누르세요. 드론은 자동으로 이륙 지점으로 되돌아갑니다.")
             }
             .tabViewStyle(PageTabViewStyle())
             .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
